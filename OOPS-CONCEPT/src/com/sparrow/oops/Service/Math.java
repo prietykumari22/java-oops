83cc5e0e14dd3ae173364caec7526360125151ee
prietykumari22 <88919236+prietykumari22@users.noreply.github.com>
2021-09-24 17:58:38 +0530

    Update Math.java

--- OOPS-CONCEPT/src/com/sparrow/oops/Service/Math.java
+++ OOPS-CONCEPT/src/com/sparrow/oops/Service/Math.java
@@ -2,7 +2,7 @@ package com.sparrow.oops.Service;
 
 public interface Math {
 	
-	/**
+	/** i switched to branch patch oops from master branch
 	 * method declaration. 
 	 * abstract method -->> for which we just know the method signature but we dont know
 	 * how to implement that.
